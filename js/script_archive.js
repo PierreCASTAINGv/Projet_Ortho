8be22f4c55f273d87b9037ff24044a53f696d694
IndigoChild <38038822+FakerEUW@users.noreply.github.com>
2018-05-18 16:23:11 +0200

    Add files via upload
    ARCHIVE AVEC IMAGE DE FOND, FICHIER CSS ET FICHIER JAVASCRIPT

---
+++ js/script_archive.js
@@ -0,0 +1,10 @@
+var panel1 = document.querySelector("ul.panel1");
+var panel2 = document.querySelector("ul.panel2");
+
+function afficherRDV() {
+    panel1.classList.toggle("panel3");
+}
+
+function afficherP() {
+    panel2.classList.toggle("panel4");
+}
