9087544c996890a2e05fcf7d1a8ce77c5abc17c4
IndigoChild <38038822+FakerEUW@users.noreply.github.com>
2018-05-23 10:21:55 +0200

    Add files via upload

--- js/script_archive.js
+++ js/script_archive.js
@@ -2,9 +2,16 @@ var panel1 = document.querySelector("ul.panel1");
 var panel2 = document.querySelector("ul.panel2");
 
 function afficherRDV() {
+    panel2.classList.remove("panel4");
+    panel1.classList.remove("panel5");
     panel1.classList.toggle("panel3");
+    panel2.classList.toggle("panel6");
+    
 }
 
 function afficherP() {
+    panel1.classList.remove("panel3");
+    panel2.classList.remove("panel6");
     panel2.classList.toggle("panel4");
+    panel1.classList.toggle("panel5");
 }
